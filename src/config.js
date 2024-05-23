43025aa4ca0b9b4397a841dd832fa4268b44ac50
real_sohel <salmanisohel26@gmail.com>
2024-05-23 13:03:57 +0530

    Front-end depoying

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL ="https://medappoint-backend.onrender.com/api/v1";// "http://localhost:5000/api/v1";
 export const token = localStorage.getItem("token");
